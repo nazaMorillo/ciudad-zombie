15bd7becb8ea6682a9118ba11e593fcfca8ce6f2
nazaMorillo <nazamorillo1@gmail.com>
2019-03-23 10:02:54 -0300

    movimiento zombie conductor

--- js/zombie_conductor.js
+++ js/zombie_conductor.js
@@ -6,19 +6,36 @@ del ZombieCaminante que eran los mismos. */
 
 var ZombieConductor = function(sprite, x, y, ancho, alto, velocidad, rangoMov, direcMov/*, parametro/s extra de ZombieConductor*/) {
   /* Completar constructor a partir de Enemigo */
+  this.direcMov=direcMov;
   Enemigo.call(this, sprite, x, y, ancho, alto, velocidad, rangoMov, direcMov);
   /* No olvidar agregar la/s propiedad/es unicas de ZombieConductor necesarias */
 }
 
 /* Completar creacion del ZombieConductor */
 ZombieConductor.prototype = Object.create(Enemigo.prototype);
+ZombieConductor.prototype.constructor = ZombieConductor;
 
 /* Completar metodos para el movimiento y el ataque */
 ZombieConductor.prototype.mover = function() {
-	if (this.direcMov=="h") {
+	/* Los movimientos estan basados en un numero aleatorio
+  La direccion horizontal es siempre la misma y va ondulando verticalmente.
+  Esto hasta llegar a sus limites, donde se invierte su direccion horizontal */
+  //console.log(this.direcMov);
+  if (this.direcMov=="h") {
     this.x -= this.velocidad;
-  } else if(this.direcMov=="v"){
+  } else if (this.direcMov=="v"){
     //Sino, hace otro movimiento
     this.y += this.velocidad;
+  }
+
+  /* En esta parte lo que hacemos es invertir la direccion horizontal si
+  toca uno de sus limites, modificando su velocidad. Si multiplicamos por -1 la
+  velocidad lo que estamos haciendo es invertir su direccion.*/
+  if ((this.x < 0) || (this.x > this.rangoMov)){
+    this.velocidad *= -1;
+  }
+  // Si sobrepasa el rangoY, lo manda al centro entre ambos rangos
+  if ((this.y < 0) || (this.y > 577)) {
+    this.velocidad *= -1;
   }
 }
